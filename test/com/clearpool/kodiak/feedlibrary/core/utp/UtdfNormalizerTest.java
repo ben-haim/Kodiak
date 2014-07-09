c181dcac9532293ebadbbd255ec9934bcfa62625
deepa.sathaye <deepa.sathaye@ny7ww7dsathaya.dimcap.corp>
2014-07-09 12:29:14 -0400

    CLEAR-2250 - removed unchecked warning on UtdfNormalizer

--- test/com/clearpool/kodiak/feedlibrary/core/utp/UtdfNormalizerTest.java
+++ test/com/clearpool/kodiak/feedlibrary/core/utp/UtdfNormalizerTest.java
@@ -14,10 +14,9 @@ import com.clearpool.kodiak.feedlibrary.core.MdFeed;
 import com.clearpool.kodiak.feedlibrary.core.MdFeedProps;
 import com.clearpool.messageobjects.marketdata.MdServiceType;
 
-
 public class UtdfNormalizerTest
 {
-	
+
 	private UtdfNormalizer normalizer;
 
 	@Before
@@ -28,12 +27,12 @@ public class UtdfNormalizerTest
 		closePrices.put("Saad", new Double(99.12));
 		MdFeedProps.putInstanceProperty(closePrices, MdFeed.UTDF.toString(), "CLOSEPRICES");
 		Map<MdServiceType, IMdLibraryCallback> callbacks = new HashMap<MdServiceType, IMdLibraryCallback>();
-		this.normalizer = new UtdfNormalizer(callbacks, "S-T",0);
+		this.normalizer = new UtdfNormalizer(callbacks, "S-T", 0);
 	}
-	
-	@SuppressWarnings({ "unchecked", "unchecked" })
+
 	@Test
-	public void testClosePrices() {
+	public void testClosePrices()
+	{
 		SaleCache cache = this.normalizer.getSalesCache();
 		assertEquals(cache.getData("Syed").getLatestClosePrice(), 100.12, 1e-15);
 		assertEquals(cache.getData("Saad").getLatestClosePrice(), 99.12, 1e-15);
